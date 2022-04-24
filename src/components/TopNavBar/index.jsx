cb0403529617e4f77183646d92b30d106879465e
Nícalo Ribeiro <nicalo63@gmail.com>
2022-04-24 20:04:32 -0300

    Ajustando estilos

--- src/components/TopNavBar/index.jsx
+++ src/components/TopNavBar/index.jsx
@@ -1,9 +1,18 @@
-import { Header } from './styles';
+import { Header, HeaderRow, BTCInfo, BTCText } from './styles';
+import { Icon } from '@iconify/react';
 
-function TopNavBar({ children }) {
+function TopNavBar({ currentPage, btcCurrency, children }) {
     return (
         <Header>
             {children}
+            <HeaderRow>
+                <h2>{currentPage === '/' ? 'Loja de pokemons' : currentPage === '/wallet' ? 'Carteira' : 'Histórico de transações'}</h2>
+                <BTCInfo>
+                    <BTCText>Última cotação: </BTCText>
+                    <Icon icon='logos:bitcoin' height={30}/> 
+                    <BTCText>${btcCurrency}</BTCText>
+                </BTCInfo>
+            </HeaderRow>
         </Header>
     );
 };
--- src/components/TopNavBar/styles.js
+++ src/components/TopNavBar/styles.js
@@ -8,3 +8,20 @@ export const Header = styled.header`
     position: relative;
     z-index: 99;
 `
+export const HeaderRow = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content:space-between;
+    align-items: center;
+    color: white;
+    padding: 0 2rem;
+`
+
+export const BTCInfo = styled.div`
+    display:flex;
+    align-items: center;
+    justify-content: center;
+`
+export const BTCText = styled.p`
+    margin: 0 10px;
+`
