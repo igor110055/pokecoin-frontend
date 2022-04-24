c8124e3a44ccb6172c4a7f47dc5b0549f81896fe
Nícalo Ribeiro <nicalo63@gmail.com>
2022-04-24 20:04:15 -0300

    Fornecendo cotacao atual do bitcoin

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import { Routes, Route, Link } from "react-router-dom";
-import { PageView } from './styles/global';
+import { useState, useEffect } from "react";
+import { Routes, Route, Link, useLocation} from "react-router-dom";
+import { PageView, LinkDiv } from './styles/global';
 import HomePage from './pages/Home';
 import WalletPage from './pages/Wallet';
 import TransactionsPage from './pages/Transactions';
 import LeftNavBar from './components/LeftNavBar';
 import TopNavBar from "./components/TopNavBar";
 import PokeCoinLogo from './assets/images/PokeCoin.png';
+import { Icon } from '@iconify/react';
+import { getBtcCurrency } from "./utils/btcutils";
 
 function App() {
+  const location = useLocation();
+  const [btcCurrency, setBtcCurrency]= useState(0);
+
+  const handleBtcCurrency = async () => {
+    try {
+      const response = await getBtcCurrency();
+      setBtcCurrency(response)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    handleBtcCurrency()
+  }, []);
+
   return (
     <div>
-      <TopNavBar>
-        <img src={PokeCoinLogo}/>
+      <TopNavBar currentPage={location.pathname} btcCurrency={btcCurrency}>
+        <img src={PokeCoinLogo} />
       </TopNavBar>
       <LeftNavBar>
-        <Link to="/">Home</Link>
-        <Link to="/wallet">Wallet</Link>
-        <Link to="/transactions">Transactions</Link>
+        <LinkDiv pageActive={location.pathname === '/'}>
+          <Link to="/"><Icon icon="ant-design:shopping-cart-outlined" width={40} /></Link>
+        </LinkDiv>
+        <LinkDiv pageActive={location.pathname === '/wallet'}>
+          <Link to="/wallet"><Icon icon="entypo:wallet" width={40} /></Link>
+        </LinkDiv>
+        <LinkDiv pageActive={location.pathname === '/transactions'}>
+          <Link to="/transactions"><Icon icon="ant-design:history-outlined" width={40} /></Link>
+        </LinkDiv>
       </LeftNavBar>
       <PageView>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<HomePage btcCurrency={btcCurrency}/>} />
           <Route path="wallet" element={<WalletPage />} />
           <Route path="transactions" element={<TransactionsPage />} />
         </Routes>
