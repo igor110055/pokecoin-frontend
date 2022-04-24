f778cfef2ed2703495c8a69045c0af2a4e7a9f52
Nícalo Ribeiro <nicalo63@gmail.com>
2022-04-24 20:05:13 -0300

    Service para BTC currency

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
 export const pokecoinApi = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
-  });
+  baseURL: process.env.REACT_APP_API_URL,
+});
+
+export const btcCurrencyApi = axios.create({
+  baseURL: process.env.REACT_APP_COINBASE_API
+})
