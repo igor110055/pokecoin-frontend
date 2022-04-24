aed6e34decca044850b9bd0f5d2ac0b6f7505366
Nícalo Ribeiro <nicalo63@gmail.com>
2022-04-24 15:04:45 -0300

    Ajustando o componente Home

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+import PokemonCard from '../../components/PokemonCard';
+import { Container } from '../../styles/global';
+import { ButtonRow, ButtonNav } from './styles';
+
+function Home() {
+    return (
+        <Container>
+            <ButtonRow>
+                <ButtonNav>Next Page</ButtonNav>
+                <ButtonNav>Prev Page</ButtonNav>
+            </ButtonRow>
+            <PokemonCard pokemonType='normal'>
+                nome
+                preco
+                comprar
+            </PokemonCard>
+        </Container>
+    )
+};
+
+export default Home;
