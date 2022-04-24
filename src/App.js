6d13d44db5740d80e0b9b7c9b3b84fcdd0e779c2
Nícalo Ribeiro <nicalo63@gmail.com>
2022-04-24 14:15:12 -0300

    Configuracao do App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import { Routes, Route, Link } from "react-router-dom";
+import { PageView } from './styles/global';
 import Home from './pages/Home';
 import Wallet from './pages/Wallet';
 import Transactions from './pages/Transactions';
-
-import './App.css';
+import LeftNavBar from './components/LeftNavBar';
+import TopNavBar from "./components/TopNavBar";
+import PokeCoinLogo from './assets/images/PokeCoin.png';
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <header>
-          <nav>
-            <Link to="/">Home</Link>
-            <Link to="/wallet">Wallet</Link>
-            <Link to="/transactions">Transactions</Link>
-        </nav>
-        </header>
+    <div>
+      <TopNavBar>
+        <img src={PokeCoinLogo}/>
+      </TopNavBar>
+      <LeftNavBar>
+        <Link to="/">Home</Link>
+        <Link to="/wallet">Wallet</Link>
+        <Link to="/transactions">Transactions</Link>
+      </LeftNavBar>
+      <PageView>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="wallet" element={<Wallet />} />
           <Route path="transactions" element={<Transactions />} />
         </Routes>
-      </div>
+      </PageView>
     </div>
   );
 }
